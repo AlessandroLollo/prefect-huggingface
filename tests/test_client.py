c8f49603cd231a35f54b07bb151c3a2956167a0d
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2023-01-29 15:46:37 +0100

    Added missing client tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -28,3 +28,73 @@ def test_get_inference_result_raises():
 
     with pytest.raises(Exception, match=msg_match):
         c.get_inference_result(model_id=model_id, inputs=inputs)
+
+
+@responses.activate
+def test_get_inference_result_with_inputs():
+    model_id = "model"
+    inputs = "test"
+    access_token = "token"
+
+    expected_result = {"result": "ok"}
+
+    c = HuggingfaceClient(access_token=access_token)
+
+    responses.add(
+        method=responses.POST,
+        url=f"https://api-inference.huggingface.co/models/{model_id}",
+        status=200,
+        json={"result": "ok"},
+    )
+
+    result = c.get_inference_result(model_id=model_id, inputs=inputs)
+
+    assert result == expected_result
+
+
+@responses.activate
+def test_get_inference_result_with_inputs_and_options():
+    model_id = "model"
+    inputs = "test"
+    options = {"options": "opt"}
+    access_token = "token"
+
+    expected_result = {"result": "ok"}
+
+    c = HuggingfaceClient(access_token=access_token)
+
+    responses.add(
+        method=responses.POST,
+        url=f"https://api-inference.huggingface.co/models/{model_id}",
+        status=200,
+        json={"result": "ok"},
+    )
+
+    result = c.get_inference_result(model_id=model_id, inputs=inputs, options=options)
+
+    assert result == expected_result
+
+
+@responses.activate
+def test_get_inference_result_with_inputs_and_parameters():
+    model_id = "model"
+    inputs = "test"
+    parameters = {"parameters": "param"}
+    access_token = "token"
+
+    expected_result = {"result": "ok"}
+
+    c = HuggingfaceClient(access_token=access_token)
+
+    responses.add(
+        method=responses.POST,
+        url=f"https://api-inference.huggingface.co/models/{model_id}",
+        status=200,
+        json={"result": "ok"},
+    )
+
+    result = c.get_inference_result(
+        model_id=model_id, inputs=inputs, parameters=parameters
+    )
+
+    assert result == expected_result
